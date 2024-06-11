7d6b99575f5171cc8aa8a506cc08a43d43815342
tayloh2 <tayloh2@mail.uc.edu>
2024-06-10 21:57:40 -0400

    Lab04_02_YearlyMaintenanceCost.iml

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double costWinter = 45;
+        double costSpring = 59;
+        double costSummer = 85;
+        double costFall = 61;
+        double yearlyCost = costWinter + costSpring + costSummer + costFall;
+        System.out.println("Your yearly maintenance cost is "  +  yearlyCost);
+    }
+}
